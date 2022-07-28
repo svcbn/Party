f7303c40697a367a85f630f627d31a3fd57f7314
J1 <gjeus0707@gmail.com>
2022-07-28 15:51:41 +0900

    Update test.cs

--- Assets/Scripts/test.cs
+++ Assets/Scripts/test.cs
@@ -7,8 +7,7 @@ public class test : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //asdfsafds
-        //asdfsdafasdfasf
+        //re0349
     }
 
     // Update is called once per frame
