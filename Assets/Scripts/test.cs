fba1f91ae2b49c55dae648f45b0a7755f37ff7d6
svcbn <qudgks7946@gmail.com>
2022-07-28 15:37:40 +0900

    07.28 수정
    test 10번째줄
    testest 16번째줄

--- Assets/Scripts/test.cs
+++ Assets/Scripts/test.cs
@@ -7,12 +7,12 @@ public class test : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //asdfsafds
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
