015a2e46520ed2760bd98f518f22e89629acb17e
svcbn <qudgks7946@gmail.com>
2022-07-29 17:16:40 +0900

    07.29 작업후

---
+++ Assets/Scenes/Test_MovewithDies.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test_MovewithDies : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int rolldice = Random.Range(0, 10);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
