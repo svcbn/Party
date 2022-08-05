20e431846d755965e949bea383254c246bfcf9ff
svcbn <qudgks7946@gmail.com>
2022-08-05 20:08:41 +0900

    0805
    작업후

--- Assets/Scripts/EventNodes.cs
+++ Assets/Scripts/EventNodes.cs
@@ -7,12 +7,37 @@ public class EventNodes : MonoBehaviour
 {
     public Route allRoutes;
     public int[] eventNodes = new int[65];
-    // Start is called before the first frame update
+    // Start is called before the first frame update9
+    public static EventNodes instance;
+
+    GameObject dArrowUp;
+    GameObject dArrowRight;
+    GameObject dArrowDown;
+    GameObject dArrowLeft;
+    GameObject ArrowUp;
+    GameObject ArrowRight;
+    GameObject ArrowDown;
+    GameObject ArrowLeft;
+
+    private void Awake() 
+    {
+        instance = this;
+
+        allRoutes = GetComponent<Route>();
+
+        FillNodeEvents();
+    }
 
     private void Start()
     {
-        allRoutes = GetComponent<Route>();
-        FillNodeEvents();
+        dArrowUp = GameObject.Find("darkArrow_up");
+        dArrowRight = GameObject.Find("darkArrow_right");
+        dArrowDown = GameObject.Find("darkArrow_down");
+        dArrowLeft = GameObject.Find("darkArrow_left");
+        ArrowUp = GameObject.Find("arrow_up");
+        ArrowRight = GameObject.Find("arrow_right");
+        ArrowDown = GameObject.Find("arrow_down");
+        ArrowLeft = GameObject.Find("arrow_left");
     }
 
     private void FillNodeEvents()
@@ -46,37 +71,178 @@ public class EventNodes : MonoBehaviour
 
     }
 
-    public void passCheck(int eventNum)
+    public int PassCheck(int currentPos)
     {
         // 루프 끊기는부분들 먼저 체크해서 연결
-        // 
-
-        switch(eventNum)        // 남은 주사위수에 +1 해줘야 되는 칸들
+        if (currentPos == 32) currentPos = 0;
+        if (currentPos == 64) currentPos = 33;
+        if (currentPos == 58) currentPos = 15;
+        return currentPos;
+    }
+    public bool PassingEvent(int Position)
+    {
+        bool canPass = true;
+        switch (eventNodes[Position])        // 남은 주사위수에 +1 해줘야 되는 칸들
         {
+
             case 6:             // 갈림길 선택
+                canPass = false;
+                StartCoroutine(SelectWay(Position));
+                MoveOnRail.Instance.steps++;
+                Debug.Log("갈림길");
                 break;
             case 7:             // 쿵쿵이 통행료
-                break;          
+                //PayTolls();
+                MoveOnRail.Instance.steps++;
+                Debug.Log("쿵쿵이");
+                break;
             case 9:             // 상점 아이템 구매
-                break;          
+                //ItemShop();
+                MoveOnRail.Instance.steps++;
+                Debug.Log("상점");
+                break;
             case 10:            // 아이템 강매
-                break;          
+                //HardSell();
+                MoveOnRail.Instance.steps++;
+                Debug.Log("강매");
+                break;
             case 11:            // 스타 구매 찬스
-                break;          
+                //BuyStar();
+                MoveOnRail.Instance.steps++;
+                Debug.Log("스타구매");
+                break;
             case 13:            // 월급
-                break;          
+                //Salary();
+                MoveOnRail.Instance.steps++;
+                Debug.Log("월급");
+                break;
             case 14:            // 플레이어 선택해서 돈뺏기
+                //TakeFromOthers();
+                MoveOnRail.Instance.steps++;
+                Debug.Log("돈뺏기");
                 break;
-            default:
-                break;
-
         }
+        return canPass;
+
     }
 
-
-    IEnumerator StopCheck()
+    IEnumerator SelectWay(int Position)
     {
 
+        if (Position == 8)
+        {
+            dArrowUp.GetComponent<SpriteRenderer>().color = new Color(255, 255, 255, 255);
+
+            dArrowRight.GetComponent<SpriteRenderer>().color = new Color(255, 255, 255, 255);
+            bool goUp = true;
+            while (true)
+            {
+                if (Input.GetKeyDown(KeyCode.W))
+                {
+                    dArrowUp.GetComponent<SpriteRenderer>().color = new Color(255, 255, 255, 0);
+                    ArrowUp.GetComponent<SpriteRenderer>().color = new Color(255, 255, 255, 255);
+                    dArrowRight.GetComponent<SpriteRenderer>().color = new Color(255, 255, 255, 255);
+                    ArrowRight.GetComponent<SpriteRenderer>().color = new Color(255, 255, 255, 0);
+                    goUp = true;
+                }
+                if (Input.GetKeyDown(KeyCode.D))
+                {
+                    dArrowRight.GetComponent<SpriteRenderer>().color = new Color(255, 255, 255, 0);
+                    ArrowRight.GetComponent<SpriteRenderer>().color = new Color(255, 255, 255, 255);
+                    dArrowUp.GetComponent<SpriteRenderer>().color = new Color(255, 255, 255, 255);
+                    ArrowUp.GetComponent<SpriteRenderer>().color = new Color(255, 255, 255, 0);
+                    goUp = false;
+                }
+
+                if (Input.GetKeyDown(KeyCode.J))
+                {
+                    if (!goUp)
+                    {
+                        MoveOnRail.Instance.routePosition = 32;
+                    }
+                    MoveOnRail.Instance.canPass = true;
+                    dArrowRight.GetComponent<SpriteRenderer>().color = new Color(255, 255, 255, 0);
+                    ArrowRight.GetComponent<SpriteRenderer>().color = new Color(255, 255, 255, 0);
+                    dArrowUp.GetComponent<SpriteRenderer>().color = new Color(255, 255, 255, 0);
+                    ArrowUp.GetComponent<SpriteRenderer>().color = new Color(255, 255, 255, 0);
+                    break;
+                }
+                yield return null;
+            }
+        }
+        if (Position == 57)
+        {
+            dArrowDown.GetComponent<SpriteRenderer>().color = new Color(255, 255, 255, 255);
+
+            dArrowLeft.GetComponent<SpriteRenderer>().color = new Color(255, 255, 255, 255);
+            bool goDown = true;
+            while (true)
+            {
+                if (Input.GetKeyDown(KeyCode.S))
+                {
+                    dArrowDown.GetComponent<SpriteRenderer>().color = new Color(255, 255, 255, 0);
+                    ArrowDown.GetComponent<SpriteRenderer>().color = new Color(255, 255, 255, 255);
+                    dArrowLeft.GetComponent<SpriteRenderer>().color = new Color(255, 255, 255, 255);
+                    ArrowLeft.GetComponent<SpriteRenderer>().color = new Color(255, 255, 255, 0);
+                    goDown = true;
+                }
+                if (Input.GetKeyDown(KeyCode.A))
+                {
+                    dArrowLeft.GetComponent<SpriteRenderer>().color = new Color(255, 255, 255, 0);
+                    ArrowLeft.GetComponent<SpriteRenderer>().color = new Color(255, 255, 255, 255);
+                    dArrowDown.GetComponent<SpriteRenderer>().color = new Color(255, 255, 255, 255);
+                    ArrowDown.GetComponent<SpriteRenderer>().color = new Color(255, 255, 255, 0);
+                    goDown = false;
+                }
+
+                if (Input.GetKeyDown(KeyCode.J))
+                {
+                    if (goDown)
+                    {
+                        MoveOnRail.Instance.routePosition = 58;
+                    }
+                    MoveOnRail.Instance.canPass = true;
+                    dArrowLeft.GetComponent<SpriteRenderer>().color = new Color(255, 255, 255, 0);
+                    ArrowLeft.GetComponent<SpriteRenderer>().color = new Color(255, 255, 255, 0);
+                    dArrowDown.GetComponent<SpriteRenderer>().color = new Color(255, 255, 255, 0);
+                    ArrowDown.GetComponent<SpriteRenderer>().color = new Color(255, 255, 255, 0);
+                    break;
+                }
+                yield return null;
+            }
+        }
+
+
     }
+    private void PayTolls()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void ItemShop()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void HardSell()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void BuyStar()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void Salary()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void TakeFromOthers()
+    {
+        throw new NotImplementedException();
+    }
+
 }
 
--- Assets/Scripts/MoveOnRail.cs
+++ Assets/Scripts/MoveOnRail.cs
@@ -10,37 +10,64 @@ public class MoveOnRail : MonoBehaviour
 
     public int steps;
 
-    int routePosition;
+    public static MoveOnRail Instance;
 
-    bool isMoving;
+    public int routePosition = 0;
+
+    public bool canPass;
+    public bool isMoving;
+
+    GameObject diceFactory;
+    private void Awake()
+    {
+        Instance = this;
+        diceFactory = (GameObject)Resources.Load("d10");
+    }
+
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space) && !isMoving)
+        if (Input.GetKeyDown(KeyCode.Space) && !isMoving)
         {
-            steps = Random.Range(1, 11);
-            Debug.Log("주사위 숫자 : " + steps);
+            StartCoroutine(RollDice());
 
-            StartCoroutine(Move());
-            
         }
+    }
+
+    IEnumerator RollDice()
+    {
+        //steps = Random.Range(1, 11);
+        //steps = 100;                      // 테스트주사위!!!!!!!!!!!!!
+        GameObject dice = Instantiate(diceFactory);
+        dice.transform.position = GameObject.Find("DicePoint").transform.position;
+        int diceNum = dice.GetComponent<Die>().value;
+        yield return new WaitForSeconds(5f);
+        // 주사위의 value 값 받아오기;
+        steps = dice.GetComponent<Die>().value;
+        Debug.Log("주사위 숫자 : " + steps);
+
+        StartCoroutine(Move());
+
     }
 
     IEnumerator Move()
     {
-        if(isMoving)
+        if (isMoving)
         {
             yield break;
         }
         isMoving = true;
+        canPass = false;
 
-        while(steps > 0)
+        while (steps > 0)
         {
+
             // 지나가면서 해야되는 이벤트 있는지(칸번호 확인)
-            
-            
-            //if (routePosition != )
+            routePosition = EventNodes.instance.PassCheck(routePosition);
+
+            canPass = EventNodes.instance.PassingEvent(routePosition);
+            yield return new WaitUntil(() => canPass);
             routePosition++;
-            
+
             Vector3 nextPos = currentRoute.childNodeList[routePosition].position;
             nextPos.y = 1.5f;
             while (MoveToNextNode(nextPos))
@@ -49,8 +76,10 @@ public class MoveOnRail : MonoBehaviour
             }
             yield return new WaitForSeconds(0.1f);
             steps--;
+
         }
         isMoving = false;
+
         // 멈춘 칸 이벤트 실행(칸번호 확인)
     }
 
@@ -59,11 +88,5 @@ public class MoveOnRail : MonoBehaviour
         return target != (transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime));
     }
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
 }
