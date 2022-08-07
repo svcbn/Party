e66eb69c02203c937457ed9c78cb3f6723ff5493
svcbn <qudgks7946@gmail.com>
2022-08-08 08:49:08 +0900

    0808
    작업전

--- Assets/Scripts/MoveOnRail.cs
+++ Assets/Scripts/MoveOnRail.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MoveOnRail : MonoBehaviour
 {
@@ -8,7 +9,7 @@ public class MoveOnRail : MonoBehaviour
 
     public float speed = 10f;
 
-    public int steps;
+    public int steps = 0;
 
     public static MoveOnRail Instance;
 
@@ -16,6 +17,21 @@ public class MoveOnRail : MonoBehaviour
 
     public bool canPass;
     public bool isMoving;
+
+    public Text stepsUI;
+
+    public int Steps
+    {
+        get
+        {
+            return steps;
+        }
+        set
+        {
+            steps = value;
+            stepsUI.text = steps + "";
+        }
+    }
 
     private void Awake()
     {
@@ -34,10 +50,10 @@ public class MoveOnRail : MonoBehaviour
     IEnumerator RollDice()
     {
         yield return null;
-        steps = Random.Range(1, 11);
-        //steps = 100;                      // 테스트주사위!!!!!!!!!!!!!
+        Steps = Random.Range(1, 11);
+        //Steps = 100;                      // 테스트주사위!!!!!!!!!!!!!
         
-        Debug.Log("주사위 숫자 : " + steps);
+        Debug.Log("주사위 숫자 : " + Steps);
 
         StartCoroutine(Move());
 
@@ -52,7 +68,7 @@ public class MoveOnRail : MonoBehaviour
         isMoving = true;
         canPass = false;
 
-        while (steps > 0)
+        while (Steps > 0)
         {
 
             // 지나가면서 해야되는 이벤트 있는지(칸번호 확인)
@@ -69,7 +85,7 @@ public class MoveOnRail : MonoBehaviour
                 yield return null;
             }
             yield return new WaitForSeconds(0.1f);
-            steps--;
+            Steps--;
 
         }
         isMoving = false;
